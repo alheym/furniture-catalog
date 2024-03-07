ec426b121281818baaeb603629bf9420d6d4b67d
alheym <alheym.mw@gmail.com>
2024-03-07 13:48:31 +0300

    feat: textures

--- src/app/item/components/content.tsx
+++ src/app/item/components/content.tsx
@@ -1,26 +1,10 @@
-import { Float } from '@react-three/drei'
 import { FC, useState } from 'react'
+import { Chair } from './Chair'
 
 const Content: FC = () => {
   const [hovered, setHover] = useState(false)
-  return (
-    <Float
-      speed={5}
-      rotationIntensity={1.5}
-      floatIntensity={1}
-      floatingRange={[1, 1.5]}
-    >
-      <mesh
-        rotation={[0, -Math.PI / 8, 0]}
-        castShadow
-        onPointerOver={() => setHover(true)}
-        onPointerOut={() => setHover(false)}
-      >
-        <boxGeometry args={[1.5, 1.5, 1.5]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-      </mesh>
-    </Float>
-  )
+
+  return <Chair />
 }
 
 export default Content
--- src/app/item/components/item.tsx
+++ src/app/item/components/item.tsx
@@ -1,11 +1,9 @@
 'use client'
 
 import {
-  AccumulativeShadows,
-  Environment,
   MeshReflectorMaterial,
   PresentationControls,
-  RandomizedLight,
+  Stage,
 } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { FC } from 'react'
@@ -15,64 +13,35 @@ const Item: FC = () => {
   return (
     <div className='w-full h-full'>
       <Canvas>
-        <color attach='background' args={['#101010']} />
-
-        <spotLight
-          position={[0, 15, 0]}
-          angle={0.3}
-          penumbra={1}
-          castShadow
-          intensity={2}
-          shadow-bias={-0.0001}
-        />
-        <ambientLight intensity={0.5} />
+        <color attach='background' args={['#1c1e20']} />
+        <fog attach='fog' args={['#1c1e20', 10, 20]} />
 
         <PresentationControls
           speed={2}
           global
-          polar={[0, Math.PI / 4]}
+          polar={[-0.1, Math.PI / 4]}
           rotation={[Math.PI / 8, Math.PI / 4, 0]}
         >
-          <Content />
+          <Stage environment='forest' intensity={1.3}>
+            <Content />
+          </Stage>
+          <mesh rotation={[-Math.PI / 2, 0, 0]} position-y={-2}>
+            <planeGeometry args={[170, 170]} />
+            <MeshReflectorMaterial
+              blur={[300, 100]}
+              resolution={2048}
+              mixBlur={1}
+              mixStrength={40}
+              roughness={1}
+              depthScale={1.2}
+              minDepthThreshold={0.4}
+              maxDepthThreshold={1.4}
+              color='#101010'
+              metalness={0.5}
+              mirror={0}
+            />
+          </mesh>
         </PresentationControls>
-
-        <AccumulativeShadows
-          temporal
-          frames={100}
-          color='orange'
-          colorBlend={2}
-          toneMapped={true}
-          alphaTest={0.75}
-          opacity={2}
-          scale={12}
-        >
-          <RandomizedLight
-            intensity={Math.PI}
-            amount={8}
-            radius={4}
-            ambient={0.5}
-            position={[5, 5, -10]}
-            bias={0.001}
-          />
-        </AccumulativeShadows>
-        <mesh rotation={[-Math.PI / 2, 0, 0]} position-y={-1}>
-          <planeGeometry args={[170, 170]} />
-          <MeshReflectorMaterial
-            blur={[300, 300]}
-            resolution={2048}
-            mixBlur={1}
-            mixStrength={20}
-            roughness={1}
-            depthScale={1.2}
-            minDepthThreshold={0.4}
-            maxDepthThreshold={1.4}
-            color='#101010'
-            metalness={0.5}
-            mirror={0}
-          />
-        </mesh>
-        {/* <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2} /> */}
-        <Environment preset='city' />
       </Canvas>
     </div>
   )
